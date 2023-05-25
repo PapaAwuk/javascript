bd7e19427f5f301c471a782eb8ea84c77c929bbf
Albert <albertawukuyoung@gmail.com>
2023-05-25 18:12:41 +0000

    Week2 assignment

---
+++ conditionals.js
@@ -0,0 +1,65 @@
+
+// let alive = false
+// let score =68;
+// // if else conditional statements
+// if(alive ==true){
+//     console.log("The Rabit is alive")
+// }
+// else{
+//     console.log("The rabit is dead")
+// }
+// // If else if conditional statements
+// if(score>=80){
+//     console.log("A+")
+// }
+// else if( score>=70 && score<=79){
+// console.log("B")
+// }else if( score>=60 && score <69){
+// console.log("C")
+// }else{
+//     console.log("D")
+// }
+
+// // Switch statements
+// let a =30
+// let b =100
+
+// switch(a+b){
+//     case 40:
+//         console.log(40)
+//     break
+//     case 80:
+//         console.log(80)    
+
+//     break
+//     case 90:
+//         console,log(90)
+//     break
+//     default:
+//         console.log("Answer does not exist")
+// }
+// // for loop
+// let number =0
+// for (let i=0;i<1000; i++){
+//     number = number+1
+//     console.log(number)
+// }
+
+// // while loop
+// let numbers =0
+// while(numbers <1000){
+// numbers = numbers+1
+// console.log(numbers)
+// }
+
+let num1=40
+let num2=120
+if(num1>num2){
+    console.log(num1+"Is the largest number")
+}
+else if(num2>num1){
+    console.log(num2 + " Is the largest number ")
+}
+else{
+    console.log( num1 + "Is equal to "+num2  )
+}
