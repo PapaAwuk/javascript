8f4972eea544b89d1fac13907f6f1186057c171c
Albert <albertawukuyoung@gmail.com>
2023-06-01 20:09:51 +0000

    assignmentsjs

---
+++ JS Class/javascript.js
@@ -0,0 +1,8 @@
+// let student ={
+//     name:'Abigail Adis',
+//     age:15,
+//     gender:'Female',
+//     Class:"Primary 6"
+// }
+
+// console.log(student ['name'])
--- conditionals.js
+++ conditionals.js
@@ -45,21 +45,40 @@
 //     console.log(number)
 // }
 
-// // while loop
-// let numbers =0
-// while(numbers <1000){
-// numbers = numbers+1
-// console.log(numbers)
+// while loop
+let numbers =0
+while(numbers <1000){
+numbers = numbers+1
+console.log(numbers)
+}
+
+// let num1=40
+// let num2=120
+// if(num1>num2){
+//     console.log(num1+"Is the largest number")
+// }
+// else if(num2>num1){
+//     console.log(num2 + " Is the largest number ")
+// }
+// else{
+//     console.log( num1 + "Is equal to "+num2  )
+//}
+
+//Function that adds three numbers
+
+// function addNum1(num1){
+//     let total1=num1
+//     return total1
+// }
+// function addNum2(num2){
+//     let total2=num2
+//     return total2
+// }
+// function addNum3(num3){
+//     let total3=num3
+//     return total3
 // }
 
-let num1=40
-let num2=120
-if(num1>num2){
-    console.log(num1+"Is the largest number")
-}
-else if(num2>num1){
-    console.log(num2 + " Is the largest number ")
-}
-else{
-    console.log( num1 + "Is equal to "+num2  )
-}
+// let total = addNum1(3) + addNum2(30) + addNum3(16);
+
+// console.log(total)
